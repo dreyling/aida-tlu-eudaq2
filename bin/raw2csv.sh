c199e830d7bf43ac195b06b7e46c3e8ad705fee9
Jan Dreyling-Eschweiler <jan.dreyling-eschweiler@desy.de>
2018-10-01 14:46:42 +0200

    delete legacy bin folder

--- bin/raw2csv.sh
+++
@@ -1,4 +0,0 @@
-echo "Please use modified euCliReader"
-
-./euCliReader -i ~/Documents/fhl-wgs01/data/data_dev/171010_aida-tlu_eudaq2_tests/run_000044_ni.raw -e 1 -E 47750 > run_000044_ni.csv
-./euCliReader -i ~/Documents/fhl-wgs01/data/data_dev/171010_aida-tlu_eudaq2_tests/run_000044_tlu.raw -e 1 -E 1091147 > run_000044_tlu.csv
